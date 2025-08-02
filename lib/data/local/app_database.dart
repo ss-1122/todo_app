46b337d16730fbdf33c0811effa222ceb168099f
ss-1122 <t815039shingo@gmail.com>
2025-08-03 01:54:48 +0900

    不要なコメントを削除

--- lib/data/local/app_database.dart
+++ lib/data/local/app_database.dart
@@ -3,9 +3,9 @@ import 'package:drift/drift.dart';
 import 'package:drift/native.dart';
 import 'package:path/path.dart' as p;
 import 'package:path_provider/path_provider.dart';
-import 'todos.dart'; // 上記テーブル定義ファイル（名前は自由）
+import 'todos.dart';
 
-part 'app_database.g.dart'; // 自動生成ファイル
+part 'app_database.g.dart';
 
 @DriftDatabase(tables: [Todos])
 class AppDatabase extends _$AppDatabase {
