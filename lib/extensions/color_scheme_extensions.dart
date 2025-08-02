62af8596891038cc3fd3c6adabd99885db9474de
ss-1122 <t815039shingo@gmail.com>
2025-08-02 23:30:11 +0900

    色定義の追加、カスタムテキストの追加、フォント設定

---
+++ lib/extensions/color_scheme_extensions.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+extension ColorSchemeExtensions on ColorScheme {
+  Color get defaultTextColor => const Color(0xFF3C3C3C);
+  Color get iconColor => const Color(0xFF6A6A6A);
+  Color get emptyBackgroundColor => const Color(0xFFF5F5F5);
+  Color get warningTextColor => const Color(0xFFB00020);
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
+import 'package:todo_app/extensions/color_scheme_extensions.dart';
+import 'package:todo_app/extensions/context_extensions.dart';
 import 'package:todo_app/router/app_routes.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -22,6 +25,13 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        textTheme: TextTheme(
+          bodyMedium: TextStyle(
+            color: context.colorScheme.defaultTextColor,
+            fontSize: 12,
+          ),
+        ),
+        fontFamily: 'NotoSansJp',
         scaffoldBackgroundColor: Colors.white,
         useMaterial3: true,
       ),
